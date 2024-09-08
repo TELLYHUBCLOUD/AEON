858a31b8a76b96728c9f7530897bfa6c29165bb6
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-09-08 13:36:22 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -270,7 +270,7 @@ def get_readable_message():
             MirrorStatus.STATUS_SEEDING,
             MirrorStatus.STATUS_PROCESSING,
         ]:
-            msg += f"\n🎡 <b>{progress_bar(download.progress())}</b> ≫ <b>{download.progress()}<b>"
+            msg += f"\n🎡 <b>{progress_bar(download.progress())}</b>  » <b>{download.progress()}</b>"
             msg += f"\n<blockquote>🔄 <b><code>Status   :</code> {download.processed_bytes()} of {download.size()}</b>"
             msg += f"\n⚡ <b><code>Speed    :</code> {download.speed()}</b>"
             msg += f'\n💣 <b><code>Estimated:</code> {download.eta()}</b>'
