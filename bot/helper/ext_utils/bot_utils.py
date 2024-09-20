d9561cae663f34cc2df782a728cfd4a5cfdef286
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2024-09-20 17:01:53 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -234,8 +234,8 @@ def progress_bar(pct):
         pct = float(pct.strip("%"))
     p = min(max(pct, 0), 100)
     c_full = int(p // 10)
-    p_str = "■" * c_full
-    p_str += "□" * (10 - c_full)
+    p_str = "▣" * c_full
+    p_str += "▢" * (10 - c_full)
     return f'<p><a href="https://t.me/tellycloud_bots">{p_str}</a></p>'
 
 def source(self):
